7ab7d8d65723839c94629c1beb5a2f4ee0138305
NeranjanK <neranjan.kuppuswamy@gmail.com>
2020-07-22 19:03:01 -0700

    Ship class without errors, need to reduce .bmp image size

--- ship_class/ship_sprite.cpp
+++ ship_class/ship_sprite.cpp
@@ -15,16 +15,16 @@ int main(int argc, char *argv[]){
 
     struct Clunky_Event event;
 
-    clunky_init(&window, &event, 640, 480);
+    clunky_init(&window, &event, 500, 500);
 
-    struct Clucky_Texture ship_tex;
+    struct Clunky_Texture ship_tex;
     clunky_load_texture("yellow_square.bmp", &ship_tex, &window);
 
     struct Clunky_Sprite ship_sprite;
     clunky_init_sprite(1, 1, &ship_tex, &ship_sprite);
 
     ship_sprite.sprite_row = 0;
-    ship_sprite.sprite_column = -1;
+    ship_sprite.sprite_column = 0;
 
     //now lets declare our Event Element Container (EEC)
     struct Clunky_Event_Element_Container *eec = (struct Clunky_Event_Element_Container *) malloc(sizeof(struct Clunky_Event_Element_Container));
@@ -36,11 +36,17 @@ int main(int argc, char *argv[]){
     cont = 1;
     unsigned long bid;
 
+    struct Clunky_Text *text = clunky_get_text(10, 10, 512, 64, 1., &window);
+    clunky_replace_text(text, "THIS IS A SHIP!\0");
+    for (int i = 0; i < text->str_used; i++){
+        printf("%c, (%d, %d)\n", text->str[i], text->str_row[i], text->str_col[i]);
+    }
+
     while(cont){
         //first thing: check to see if there have been any new events!
         clunky_event(&event);
         clunky_eec_update(eec, &event, &window);
-        //clunky_render_text(text, &window);
+        clunky_render_text(text, &window);
         
         //if there has been a keypress (indicated with num_input != 0)
         //or a mouse click (indicated with lc || rc == 1)
@@ -80,12 +86,15 @@ int main(int argc, char *argv[]){
             }
         }
 
-        /*
-        for (int i = 0; i < 1*64; i += 64){
+        
+        /*for (int i = 0; i < 1*64; i += 64){
             for (int j = 0; j < 1*64; j += 64){
-               clunky_animate_sprite(100 + i, 100 + j, &water_spr, &window);
+               clunky_animate_sprite(100 + i, 100 + j, &ship_sprite, &window);
             }
         }*/
+        clunky_render_sprite(30, 30, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
+        clunky_render_sprite(60, 60, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
+        //clunky_render_sprite(100, 100, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
 
         //Update the window!
         clunky_present_window(&window);
