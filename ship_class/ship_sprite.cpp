f40e805bc07d16b102bc3a3be44c43af822ec2f1
NeranjanK <neranjan.kuppuswamy@gmail.com>
2020-07-27 23:41:12 -0700

    Ship class now has functionality to snap too

--- ship_class/ship_sprite.cpp
+++ ship_class/ship_sprite.cpp
@@ -8,36 +8,84 @@
 #include "../clunky_gl/clunkyEventElements.h"
 
 int main(int argc, char *argv[]){
+
+    //dec common use variables
     int i, j, k;
     int cont;
 
-    struct Clunky_Window window; 
+    //create a clunky window object.
+    //this will be the window that we will render to
+    struct Clunky_Window window;
 
+    //we need to create our event handler!
     struct Clunky_Event event;
 
-    clunky_init(&window, &event, 500, 500);
+    //now we will init the window, this will set the width and height of the
+    //window, as well as call the nessisary SDL2 function calls for us
+    //this has to be done first, as it inits SDL2 functions
+    clunky_init(&window, &event, 640, 480);
 
+    //Now we will load the texture that contains all of our main menu buttons
+    struct Clunky_Texture water_tex;
+    clunky_load_texture("./clunky_assets/Water.bmp", &water_tex, &window);
+
+    //From the trexture we can create a sprite. 
+    //Clunky Sprites allow for the rendering of partial sections of
+    //the texture, instead of the entire texture at once
+    struct Clunky_Sprite water_spr;
+    //when initing a sprite, we need to give it how many ROWS and COLUMNS there
+    //are on the sprite sheet. it will do the rest
+    clunky_init_sprite(1, 5, &water_tex, &water_spr);
+
+    water_spr.sprite_row = 0;
+    water_spr.sprite_column = -1;
+    clunky_sprite_scale(1.75, &water_spr);
+
+    //create a 5x5 board for now
+    char names[] = "cell_x_\0";
+    int cnt = 0;
+    struct Clunky_Event_Element **cells = (struct Clunky_Event_Element **) malloc(sizeof(struct Clunky_Event_Element *) * 25);
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 5; j++){
+            names[4] = '0' + i;
+            names[6] = '0' + j;
+
+            cells[cnt] = (struct Clunky_Event_Element *) malloc(sizeof(struct Clunky_Event_Element));
+
+            clunky_element_init(cells[cnt], &water_spr, water_spr.ap_w*i, water_spr.ap_h*j, 0, names, 'S', 'A');
+
+            cnt++;
+        }
+    }
+
+     //Now we will load the texture that contains all of our main menu buttons
     struct Clunky_Texture ship_tex;
-    clunky_load_texture("rsz_yellow_square.bmp", &ship_tex, &window);
-
+    clunky_load_texture("/rsz_yellow_square.bmp", &ship_tex, &window);
     struct Clunky_Sprite ship_sprite;
     clunky_init_sprite(1, 1, &ship_tex, &ship_sprite);
 
-    ship_sprite.sprite_row = 0;
-    ship_sprite.sprite_column = 0;
+    struct Clunky_Event_Element *ship = clunky_dragable_element_init(&ship_sprite, 500, 200, 0, "ship");
+
+
 
     //now lets declare our Event Element Container (EEC)
     struct Clunky_Event_Element_Container *eec = (struct Clunky_Event_Element_Container *) malloc(sizeof(struct Clunky_Event_Element_Container));
 
     //init the eec
     clunky_eec_init(eec);
-
+    printf(">>>>>\n");
+    clunky_eec_add_elements(eec, cells, 25);
+    clunky_eec_add_elements(eec, &ship, 1);
+    printf("ASASASASAS\n");
     //now we can begin our main loop!
     cont = 1;
     unsigned long bid;
 
-    struct Clunky_Text *text = clunky_get_text(10, 10, 512, 64, 1., &window);
-    clunky_replace_text(text, "THIS IS A SHIP!\0");
+    //create a clunky text object!
+    struct Clunky_Text *text = clunky_get_text(10, 10, 512, 64, 0.5, &window);
+    printf("HHH\n");
+    clunky_replace_text(text, "THIS IS THE BOARD! :)\0");
+    printf("CCCC\n");
     for (int i = 0; i < text->str_used; i++){
         printf("%c, (%d, %d)\n", text->str[i], text->str_row[i], text->str_col[i]);
     }
@@ -47,7 +95,6 @@ int main(int argc, char *argv[]){
         clunky_event(&event);
         clunky_eec_update(eec, &event, &window);
         clunky_render_text(text, &window);
-        
         //if there has been a keypress (indicated with num_input != 0)
         //or a mouse click (indicated with lc || rc == 1)
         //then check to see what has happened
@@ -68,40 +115,24 @@ int main(int argc, char *argv[]){
 
         //check the buttons to see if anywhere clicked
         //first, check to make sure that there was a selected button
-        if (eec->sum.eid != 0){
-            //ok, a button was interacted with!
-            //find out which one
-            if (eec->sum.eid == clunky_hash_gen("start")){
-                printf("START\n");
-            }
-            else if (eec->sum.eid == clunky_hash_gen("options")){
-                printf("OPTIONS\n");
-            }
-            else if (eec->sum.eid == clunky_hash_gen("credits")){
-                printf("CREDITS\n");
-            }
-            else if (eec->sum.eid == clunky_hash_gen("quit")){
-                printf("QUIT\n");
-                cont = 0;
+        if (eec->sum.event_type != 'N'){
+            if (eec->sum.event_type == 'S'){
+                printf("Elements have been snapped together!\n");
+                for (int k = 0; k < eec->num_snaps; k++){
+                    if (eec->snaps[k]->eid == eec->sum.eid_two){
+                        printf(">>Snap To Element: %s\n", eec->snaps[k]->name);
+                    }
+                }
             }
         }
 
-        
-        /*for (int i = 0; i < 1*64; i += 64){
-            for (int j = 0; j < 1*64; j += 64){
-               clunky_animate_sprite(100 + i, 100 + j, &ship_sprite, &window);
-            }
-        }*/
-        clunky_render_sprite(100, 100, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
-        clunky_render_sprite(164, 100, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
-        //clunky_render_sprite(100, 100, ship_sprite.sprite_row, ship_sprite.sprite_column, &ship_sprite, &window);
 
         //Update the window!
         clunky_present_window(&window);
     }
+    
 
-
-
+    //Quit SDL subsystems
     SDL_Quit();
 
     return 0;
