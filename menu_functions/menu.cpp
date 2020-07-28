fb3cf6c0d258cf143bd5f036efbdf6e072d1d8ca
David Eaton <eatonda@oregonstate.edu>
2020-07-28 17:09:41 -0600

    update menu_functions folder

--- menu_functions/menu.cpp
+++ menu_functions/menu.cpp
@@ -8,8 +8,11 @@
 
 #include "menu.hpp"
 
-
-Menu::Menu(struct Clunky_Window* window, Clunky_Event_Element_Container* menuOptions, int numOfOptions, int* values, std::string* optionNames, Clunky_Text* title) {
+/*  Menu
+ Description: Typical menu constructor with no additional text content.
+ Parameters: struct Clunky_Window* window, Clunky_Event_Element_Container* menuOptions, int numOfOptions, int* values, std::string* optionNames, Clunky_Text* title
+ */
+Menu::Menu(struct Clunky_Window* window, struct Clunky_Event_Element_Container* menuOptions, int numOfOptions, int* values, std::string* optionNames, struct Clunky_Text* title) {
     this->window = window;
     this->menuOptions = menuOptions;
     this->numOfOptions = numOfOptions;
@@ -18,6 +21,21 @@ Menu::Menu(struct Clunky_Window* window, Clunky_Event_Element_Container* menuOpt
     this->title = title;
 }
 
+/* Menu
+ Description: Menu constructor when utilizing a menu as a page with additional text content
+ Parameters:
+ */
+Menu::Menu(struct Clunky_Window* window, Clunky_Event_Element_Container* menuOptions, int numOfOptions, std::string* optionNames, struct Clunky_Text* title, struct Clunky_Text** content, int numberOfPages, int linesPerPage) {
+    this->window = window;
+    this->menuOptions = menuOptions;
+    this->numOfOptions = numOfOptions;
+    this->optionNames = optionNames;
+    this->title = title;
+    this->content = content;
+    this->numberOfPages = numberOfPages;
+    this->linesPerPage = linesPerPage;
+    this->pageNumber = 0;
+}
 
 struct Clunky_Event_Element_Container* Menu::getMenuOptions() {
     return menuOptions;
@@ -44,6 +62,34 @@ void Menu::_display(struct Clunky_Event* event) {
     //too many resources
     //SDL_Delay(10);
     
+}
+
+/*  _displayPage
+ Description: Page version of menu display
+ Parameters:
+ Preconditons: event != NULL and pageNum >= 0 pageNum < numberOfPages
+ */
+void Menu::_displayPage(struct Clunky_Event* event, int pageNum){
+    // Assert preconditions
+    assert(event != NULL);
+    assert(pageNum >= 0 && pageNum < numberOfPages);
+    
+    //Update and present the window
+    clunky_present_window(window);
+    clunky_render_text(title, window);
+    
+//    for (int i = 0; i < linesPerPage; i++) {
+//        clunky_render_text(&(content[pageNumber][i]), window);    // Render page to screen
+//    }
+    //std::cout << "displayPage() pageNumber = " << pageNumber << std::endl;
+    if (pageNumber > 0) {
+        for (int i = linesPerPage * pageNumber; i < linesPerPage * (pageNumber + 1); i++){
+            clunky_render_text(content[i], window);
+        }
+    } else if (pageNumber == 0){
+        for (int i = 0; i < linesPerPage; i++){
+            clunky_render_text(content[i], window);
+        }    }
 }
 
 /*  run
@@ -96,5 +142,55 @@ int Menu::run(struct Clunky_Event* event, int* userInput) {
 }
         
             
-        
+
+/*  runPage
+ Description: Pager menu version of run. Like previous run function but doesn't need to update a userinput variable. It simply returns when back to main menu is selected, and returns 0 if so.
+ Parameters: struct Clunky_Event* event
+ Preconditions: event cannot be NULL and member variables cannot be null. Also number of pages >= 0, linesPerPage must be greater than 0.
+ Returns: -1 if SDL_QUIT is encountered, 0 if 'BACK TO MAIN' is selected
+*/
+int Menu::runPage(struct Clunky_Event* event) {
+    // asset preconditions
+    assert(content != NULL);
+    assert(event != NULL);
+    assert(numberOfPages >= 0 && linesPerPage > 0);
+
+    int userChoice = -2;    //Stores user's selection
+
+    while(true) {
+        //NOTE: Before checking the event structure to see if
+        //there are any events in it, you must fisrt call clunky_event to
+        //check the system for events and populate the structure
+        clunky_event(event);
+        clunky_eec_update(menuOptions, event, window); //update event container
+        // Sift through events if there are any.
+        if (event->num_input) {
+
+            // Check for SDL_QUIT
+            if (_SDL_QUIT_Check(event) < 0) {
+                return -1;  // Return -1 to signal exit from SDL_QUIT
+            }
+        }
+
+        userChoice = _getMouseClick(menuOptions, numOfOptions, optionNames, event);
+
+        if (userChoice == numOfOptions) {
+            return 0; // MAIN MENU  was selected, return 0
+
+        } else if (userChoice == -1) {
+            // Previous page was selected.
+            if (pageNumber - 1 >= 0) {
+                pageNumber--;   //If not first page decrement index
+            }
+        } else if (userChoice == numOfOptions + 1) {
+            // Next page was selected
+            if (pageNumber + 1 < numberOfPages) {
+                pageNumber++;   // If not last page increment page number
+            }
+        }
+
+         _displayPage(event, pageNumber);   // Call pager version of _display
+
+    }
+}
 
