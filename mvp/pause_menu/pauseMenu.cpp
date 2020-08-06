ece9a48d78b33d8996326a6c0f4d8970c79f49f3
Doug Hughes <dhughes8906@gmail.com>
2020-08-06 14:12:13 -0400

    Implements the user manual within the pause menu

--- mvp/pause_menu/pauseMenu.cpp
+++ mvp/pause_menu/pauseMenu.cpp
@@ -85,6 +85,74 @@ int getPauseSelection(struct Clunky_Window* window, struct Clunky_Event* event) 
 	// 3 for exit menu
 	int returnVal = -1;
 
+
+    /* CREATE HELP MENU */
+	const int HELP_MENU_NUM_OF_BUTTONS = 3;
+    std::string helpTitle = "USER MANUAL";
+    
+    // Note main menu must be capitalized to MAIN MENU for menu function to recognize it properly
+    std::string helpMenuDescriptions [HELP_MENU_NUM_OF_BUTTONS] = {"MAIN MENU", "PREVIOUS", "NEXT"};
+    
+    struct Clunky_Texture help_menu_texture;
+    clunky_load_texture(toC_String("./clunky_assets/pauseUserManualButtons.bmp"), &help_menu_texture, window);  // Initialize texture for board menu
+    
+    struct Clunky_Sprite hmb;   //stores sprite image of help menu buttons
+    clunky_init_sprite(HELP_MENU_NUM_OF_BUTTONS, NUM_OF_SPRITE_COLS, &help_menu_texture, &hmb); // Initialize sprite
+    
+    // Buttons for pages should be at the bottom of the screen in a single row
+    int xOffset = window->width * 0.25;
+    x = window->width * 0.20;
+    for (int i = 0; i < HELP_MENU_NUM_OF_BUTTONS; i++){
+        xCoordinates[i] = x;
+        x+= xOffset;
+        yCoordinates[i] = window->height * 0.95;
+    }
+   
+	// store buttons as an array of Clunky_Button 
+    struct Clunky_Event_Element_Container* help_menu = buttonSetup(HELP_MENU_NUM_OF_BUTTONS, helpMenuDescriptions, &hmb, xCoordinates, yCoordinates);    
+    struct Clunky_Text* helpMenuTitle = clunky_get_text(window->width * 0.4, window->height * 0.15, window->width * 0.50, window->height * 0.10, 1.0, window);    // Get Clunky_Text instance for help menu title
+    
+    clunky_add_text(helpMenuTitle, toC_String(helpTitle));   // Add text to clunky_text
+    
+//    struct Clunky_Text* content = (struct Clunky_Text*)malloc(sizeof(struct Clunky_Text*) * 8);
+   std::string words[8] = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    
+    std::string path = "./menu_functions/menu_assets/user_manual.txt";
+    std::vector<char*> manualStrings;
+    //printf("|||||||||\n");
+    fileToStrings(path, manualStrings);    //Get cstring version of text file
+   // printf("|||||||||\n");
+    
+    // Trace statement
+    for(int i = 0; i < manualStrings.size(); i ++){
+        printf("GGP() %s\n", manualStrings[i]);
+    }
+
+ 
+    struct Clunky_Text* help_content[manualStrings.size()];
+    int lineOffset = window->height * 0.05;
+    int lineY = window->height * 0.3;
+    int linesPerPage = 2;   //To show multipage functionality
+    int count = 0;
+    for(int i = 0; i < manualStrings.size(); i++) {
+        if (count >= linesPerPage) {
+            count = 0;  //Reset count
+            lineY = window->height * 0.3;   //Reset y
+        }
+        count++;
+        printf(">>%d, %d\n", i, manualStrings.size());  // Trace statement
+        help_content[i] = clunky_get_text(window->width * 0.05, lineY, window->width * 0.9, window->height * 0.10, 1.0, window);
+        //printf("===\n");
+        //clunky_add_text(content[i], toC_String(words[i]));
+        clunky_add_text(help_content[i], manualStrings[i]);
+        //printf("++++\n");
+        lineY+= lineOffset;
+    }
+    
+    Menu* helpMenu = new Menu(window, help_menu, HELP_MENU_NUM_OF_BUTTONS, helpMenuDescriptions, helpMenuTitle, help_content, 4, linesPerPage);	
+
+
+
 	while (menuCont) {
 		menuFlag = pMenu->run(event, &navigation); 
 		if (menuFlag > 0) {
@@ -92,9 +160,10 @@ int getPauseSelection(struct Clunky_Window* window, struct Clunky_Event* event) 
 				// user manual selected
 				case 0: 
 					std::cout << "Pause Menu: User Manual\n";
-					// ** once game manual is finalized there should be
-					// functionality to go to the game manual and return to
-					// the pause menu **
+					if (helpMenu->runPage(event) == -1) {
+						menuCont = 0;
+					}				
+	
 					break;
 				// restart game selected
 				case 1:
@@ -125,7 +194,6 @@ int getPauseSelection(struct Clunky_Window* window, struct Clunky_Event* event) 
 		}
 	}
 
-	SDL_QUIT;
 	// free menu memory
 	free(pMenu);
 
